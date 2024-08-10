19e6af6ddc72d457b17cae7f2c3b65ab727768e5
frozflame <frozflame@outlook.com>
2024-08-10 16:46:23 +0800

    ver 0.3.1: fix exports: defaultCellComponentMap, Td

--- src/index.tsx
+++ src/index.tsx
@@ -2,8 +2,7 @@ import "./styles/main.scss";
 
 export type {CellProps, Column, PageData, APIPageData, QueryParams, APIQueryParams, Row} from "./types";
 export {translateQueryParams, untranslatePageData} from "./types";
-export type {LinkDict, defaultCellComponentMap} from "./cells";
-export type {Td} from "./table";
+export {type LinkDict, defaultCellComponentMap} from "./cells";
 export {Grid, type GridProps, type GridOptions, type GridWires} from "./grid";
-export {Table} from "./table";
+export {Table, Td} from "./table";
 export {SimpleGrid, type SimpleGridProps, type SimpleGridOptions} from "./simple";
